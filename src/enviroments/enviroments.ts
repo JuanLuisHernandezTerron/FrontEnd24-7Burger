27610ca7ff3dd7abcd8b0c989b2ff806c68a6e9e
unknown <you@example.com>
2023-09-05 02:33:08 +0200

    cambios

--- src/enviroments/enviroments.ts
+++ src/enviroments/enviroments.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    url: 'https://backend-247burger.onrender.com/',
+    url: 'https://backend-247burger.onrender.com',
     id_tienda: '64c68843533be3fea571a2b5'
 };
